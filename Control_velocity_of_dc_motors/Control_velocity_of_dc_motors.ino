ae84968d879645467a703619736b451ae4a306e7
Aïda Calero <aida.calero@uvic.cat>
2023-03-14 18:20:55 +0100

    program to control dc motors arduino

--- Control_velocity_of_dc_motors/Control_velocity_of_dc_motors.ino
+++ Control_velocity_of_dc_motors/Control_velocity_of_dc_motors.ino
@@ -1,20 +1,79 @@
-const int pinENA = 8;
-const int pinIN1 = 10;
-const int pinIN2 = 9;
+//#include <LEANTEC_ControlMotor.h>   //Incluimos la librería control de motores 
+// Configuramos los pines que vamos a usar
+//ControlMotor control(2,3,7,4,5,6);  
+int MotorDer1=2; //El pin 2 de arduino se conecta con el pin In1 del L298N 
+int MotorDer2=3; //El pin 3 de arduino se conecta con el pin In2 del L298N 
+int MotorIzq1=7; //El pin 7 de arduino se conecta con el pin In3 del L298N 
+int MotorIzq2=4; //El pin 4 de arduino se conecta con el pin In4 del L298N 
+int PWM_Derecho=5; //El pin 5 de arduino se conecta con el pin EnA del L298N 
+int PWM_Izquierdo=6; //El pin 6 de arduino se conecta con el pin EnB del L298N 
+int velocidad=150; //Declaramos una variable para guardar la velocidad 
 
-const int speed = 200;      //velocidad de giro 80% (200/255)
+void setup()  {    //Configuramos los pines como salida
+   pinMode(MotorDer1, OUTPUT);    pinMode(MotorDer2, OUTPUT);
+   pinMode(MotorIzq1, OUTPUT);    pinMode(MotorIzq2, OUTPUT);
+   pinMode(PWM_Derecho, OUTPUT);   pinMode(PWM_Izquierdo, OUTPUT); 
+}  
 
-void setup()
-{
-   pinMode(pinIN1, OUTPUT);
-   pinMode(pinIN2, OUTPUT);
-   pinMode(pinENA, OUTPUT);
+void derecha_antihorario_izquierda_horario()
+{  
+   /*En esta fución la rueda derecha girará en sentido antihorario y la izquierda en sentido horario. En este caso, si las ruedas estuvieran en el chasis de un robot, el robot retrocederia.*/   
+  digitalWrite(MotorDer1,HIGH);   
+  digitalWrite(MotorDer2,LOW);
+  digitalWrite(MotorIzq1,HIGH);   
+  digitalWrite(MotorIzq2,LOW);
+  analogWrite(PWM_Derecho,50);//Velocidad motor derecho 200
+  analogWrite(PWM_Izquierdo,200);//Velocidad motor izquierdo 200 
+} 
+
+void derecha_horario_izquierda_antihorario(){
+  /*En esta fución la rueda derecha girará en sentido horario y la izquierda en sentido antihorario. En este caso, si las ruedas estuvieran en el chasis de un robot, el robot avanzaría.*/     
+  digitalWrite(MotorDer1,LOW);   
+  digitalWrite(MotorDer2,HIGH);   
+  digitalWrite(MotorIzq1,LOW);   
+  digitalWrite(MotorIzq2,HIGH);
+  analogWrite(PWM_Derecho,200);//Velocidad motor derecho 200
+  analogWrite(PWM_Izquierdo,200);//Velocidad motor izquierdo 200 
+} 
+
+void giro_horario(){
+  /*En esta fución ambas ruedas girarán en sentido horario. En este caso, si las ruedas estuvieran en el chasis de un robot, el robot giraria a la derecha.*/   
+  digitalWrite(MotorDer1,HIGH);   
+  digitalWrite(MotorDer2,LOW);   
+  digitalWrite(MotorIzq1,LOW);   
+  digitalWrite(MotorIzq2,HIGH);
+  analogWrite(PWM_Derecho,200);//Velocidad motor derecho 200
+  analogWrite(PWM_Izquierdo,200);//Velocidad motor izquierdo 200 
 }
 
-void loop()
-{
-   digitalWrite(pinIN1, HIGH);
-   digitalWrite(pinIN2, LOW);
-   analogWrite(pinENA, speed);
-   delay(1000);
+void giro_antihorario(){  
+  /*En esta fución ambas ruedas girarán en sentido antihorario. En este caso, si las ruedas estuvieran en el chasis de un robot, el robot giraria a la izquierda.*/   
+  digitalWrite(MotorDer1,LOW);   
+  digitalWrite(MotorDer2,HIGH);   
+  digitalWrite(MotorIzq1,HIGH);   
+  digitalWrite(MotorIzq2,LOW);
+  analogWrite(PWM_Derecho,200);//Velocidad motor derecho 200
+  analogWrite(PWM_Izquierdo,200);//Velocidad motor izquierdo 200 
+} 
+
+void parar(){  /*Función para que las ruedas paren*/
+  digitalWrite(MotorDer1,LOW);   
+  digitalWrite(MotorDer2,LOW);   
+  digitalWrite(MotorIzq1,LOW);   
+  digitalWrite(MotorIzq2,LOW);
+  analogWrite(PWM_Derecho,200);//Velocidad motor derecho 200
+  analogWrite(PWM_Izquierdo,200);//Velocidad motor izquierdo 200 
 }
+
+void loop()  {  
+  giro_horario();  //Llamamos a la función giro_horario
+  delay(3000);     //Durante 3 segundos ejecutamos esa función
+  giro_antihorario(); //Llamamos a la función giro_antihorario
+  delay(3000);     //Durante 3 segundos ejecutamos esa función
+  derecha_horario_izquierda_antihorario();  //Llamamos a la función derecha_horario_izquierda_antihorario 
+  delay(3000);     //Durante 3 segundos ejecutamos esa función
+  derecha_antihorario_izquierda_horario(); //Llamamos a la función derecha_antihorario_izquierda_horario 
+  delay(3000);     //Durante 3 segundos ejecutamos esa función
+  parar();         //Llamamos a la función parar 
+  delay(3000);     //Durante 3 segundos ejecutamos esa función   
+} 
